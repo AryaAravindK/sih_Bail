34ed63f49daa3fa4dbdad9fab0a42a0c485fef41
AryaAravindK <khadedarya@gmail.com>
2024-09-16 01:02:52 +0530

    added dependancies

--- Back_end/backend/home/models.py
+++ Back_end/backend/home/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Offences(models.Model):
     title = models.CharField(max_length=100 )
-    penal_code = models.CharField(max_length=7)
+    penal_code = models.CharField(max_length=10)
     max_penalty = models.CharField(max_length=255)
     min_penalty = models.CharField(max_length=255)
     bailable = models.BooleanField()

e67152fe2e4a50f422f8839040d090acce7adb98
AryaAravindK <khadedarya@gmail.com>
2024-09-15 22:15:57 +0530

    added models

--- Back_end/backend/home/models.py
+++ Back_end/backend/home/models.py
@@ -2,3 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+
+class Offences(models.Model):
+    title = models.CharField(max_length=100 )
+    penal_code = models.CharField(max_length=7)
+    max_penalty = models.CharField(max_length=255)
+    min_penalty = models.CharField(max_length=255)
+    bailable = models.BooleanField()
+    compoundable = models.BooleanField()
+    
+
+    def __str__(self):
+        return f"{self.penal_code} - {self.title}"
+    

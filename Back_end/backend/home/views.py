8fabd7aed7814fd10f44a18a87e488e4aecb1130
AryaAravindK <khadedarya@gmail.com>
2024-09-16 16:45:05 +0530

    added eligibility route

--- Back_end/backend/home/views.py
+++ Back_end/backend/home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Offences
 
 # Create your views here.
 
 def home(request):
     return render(request,'home.html')
+
+
+def eligibility(request):
+    offences = Offences.objects.all()
+    return render(request,'eligibility.html',{'offences':offences})

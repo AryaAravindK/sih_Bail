467734ea32dc22b1c4e47d4d3693460033882215
AryaAravindK <khadedarya@gmail.com>
2024-09-30 02:12:31 +0530

    building chatbot

--- Back_end/backend/chatbot/views.py
+++ Back_end/backend/chatbot/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import os
+import google.generativeai as genai
 # Create your views here.
+
+genai.configure(api_key = os.getenv("GEMINI_API_KEY"))
+
+
+def chatbot_view(request):
+    
+    if request.method == 'POST':
+        user_message = request.POST.get('message')
+        
+        response = genai.text(message = user_message)
+        
+        return JsonResponse({'message':user_message,'response':response})
+    
+    return render(request,'chatbot/chatbot.html')
+    
+    
